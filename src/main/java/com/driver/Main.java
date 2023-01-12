4edba86148d25a487d2b121e8ef8e2430b2f2bd0
Chirag <chiragkath7@gmail.com>
2023-01-13 04:05:21 +0530

    Encapsulation-Solution 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r=new RWOnly();
+//        r.name="Chirag";
+//        System.out.println(r.name);
+//        java: name has private access in com.driver.RWOnly
+        r.setName("Chirag");
+        System.out.println(r.getName());
+    }
 }
